e5184669e328e31b93768900a9b41c8a7e3ef14e
Manole-Adrian <manoleadrian1997@gmail.com>
2022-08-24 12:16:59 +0300

    turned sticky title into normal title

--- trivia-game/src/Components/Questions/StickyTitle/StickyTitle.js
+++ trivia-game/src/Components/Questions/StickyTitle/StickyTitle.js
@@ -7,6 +7,6 @@ export default function StickyTitle() {
 
     return(<div className="stickyTitleContainer">
         <h1> Comp<span className="orange">Tri</span></h1>
-        <h2>By Manole Adrian</h2>
+        <h2>The Computer Science Trivia Game!</h2>
     </div>)
 }
